b08c9f40ad9d32ce3de3f6fcf21e1cc33249d791
crankyastrologer <ansh21csu519@ncuindia.edu>
2024-07-26 18:17:52 +0530

    add dash app with data visualization

---
+++ Dash/api.py
@@ -0,0 +1,56 @@
+from dash import Dash, html, dcc, callback, Output, Input
+import plotly.express as px
+import pandas as pd
+import plotly.graph_objs as go
+from sklearn.preprocessing import MinMaxScaler
+from views import dic
+
+df = pd.read_csv('../processed.csv')
+other_columns = ['gdp_growth', 'house_inventory', 'Diff_house_inventory',
+                 'inflation', 'material_cost', 'mortgage_rate', 'population_growth',
+                 'shares', 'Unemployment', 'vacancy_rate', 'Total', 'rent_price']
+base_columns = ['house_price', 'house_price_diff']
+app = Dash()
+
+app.layout = [
+    html.H1(children='Title of Dash App', style={'textAlign': 'center'}),
+    dcc.Dropdown(other_columns, 'gdp_growth', id='dropdown-selection'),
+    dcc.Graph(id='graph-content'),
+    html.P(id='info', children="hello world")
+]
+
+
+@callback(
+    Output('graph-content', 'figure'),
+    Input('dropdown-selection', 'value')
+)
+def update_graph(value):
+    columns_to_plot = base_columns + [value]
+    scaler = MinMaxScaler()
+    df_normalized = pd.DataFrame(scaler.fit_transform(df[columns_to_plot]), columns=columns_to_plot)
+    df_normalized['DATE'] = df['DATE']
+    fig = go.Figure()
+    for column in columns_to_plot:
+        fig.add_trace(go.Scatter(x=df_normalized['DATE'], y=df_normalized[column], mode='lines', name=column))
+    fig.update_layout(
+        title="Normalized Time Series Plot",
+        xaxis_title="Date",
+        yaxis_title="Normalized Values",
+        legend_title="Variables",
+        height=600,
+        width=900
+    )
+
+    return fig
+
+
+@callback(
+    Output('info', 'children'),
+    Input('dropdown-selection', 'value')
+)
+def update_info(value):
+    return dic[value]
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ Dash/views.py
@@ -0,0 +1,42 @@
+dic = {
+    "gdp_growth" : '''There seems to be some correlation between GDP growth and house price changes, 
+    particularly during major economic events (2008-2009 crisis, 2020 pandemic).\n
+    Note: the GDP growth data is taken yearly while others are monthly and growth data is only available till 2023''',
+    "population_growth" : '''There doesn't seem to be much correlation between the populatoin growth and house prices 
+    at least in short term \n
+    Note: population growth data is only available till 2023 and has yearly frequency''',
+    'inflation':'''There seems to be a correlation between inflation and house prices especially in recent years. \n
+    During the housing crash of 2008 the inflation and house prices both went down but the inflation rate recovered much 
+    quicker than the house prices probably due the stimulus by the government''',
+    'material_cost':'''Over long term material cost is mostly stable with gradual increase but in recent years especially during 
+    the pandemic there is a huge spike which seems to correlate with the increase in house prices''',
+    'house_inventory': '''There seems to be some seasonal variation as the inventory dips around the start of the year 
+    but overall we can see there is a huge dip during the pandemic which coincides with the increase in prices and even since then
+    the inventory hasn't reached the pre covid levels\n
+    Note: the data for house inventory is only available from June, 2016''',
+    'rent_price' : '''The rent prices have seen a steady growth and the effect of the financial crash was much tamer\n
+    but starting the pandemic there is a huge increase in rent prices making housing a more productive asset''',
+    'vacancy_rate' : '''There has been a steady decline in vacany rate from the financial crash hinting a possible mismatch 
+    between the supply and demand which seems to have reach some sort of peak during the pandemic but such lower vacancy rates
+    suggest that landlords can raise rent without fearing the loss of tenants as there are not many places that are vacant''',
+    'mortgage_rate' : '''Mortgage rates seem to be inversely proportional to the house prices atleast before the
+     pandemic where there is a slowdown increase but not complete slow.\n
+     This may be due to the fact as a person has some sort of budget which cannot be exceeded and as most of them don't pay
+     in full the mortgage also adds to the full price hence when the rates increases the price of house seen to decrease 
+     or at least has a gradual increase as demand outpaces supply but the lower interest rates during pandemic lead to 
+     lower mortgage rates and hence the price of house also increased''',
+    'Total': '''This is the graph of the total houses build in US as you can see there is some seasonal variation in the 
+    no of houses built per year\n
+    But the more interesting thing is that the no of houses built reached a peak before the financial crash and dropped 
+    precipitously since then and to date hasn't recovered creating a supply shortage \n,
+    Note: The data is only available till end of 2023
+    ''',
+    'unemployment': '''There seems to be some  inverse correlation between the unemployment and house prices
+    some factors such as high enough demand or low enough supply can overturn that \n
+    Note: The unemployment data is only available from June, 2004''',
+    'shares': '''Stock market and housing prices goes hand in hand for most part both being affected by the financial crash 
+    and the pandemic looks like they both are affected by how good the economy is doing and with the recent increase in institutional
+    investment in housing market this correlation will probably increase.'''
+
+
+}
